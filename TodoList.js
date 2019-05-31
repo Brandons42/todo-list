05b675e904e69adc537cd4ed29262bc344ae8d29
Brandon <brandonbsuen@gmail.com>
2019-05-31 06:52:20 -0700

    Functional and designed

--- TodoList.js
+++ TodoList.js
@@ -49,33 +49,39 @@ export default class TodoList extends React.Component {
 
 	render() {
 		return (
-			<div className=''>
-				<h1>Todo-list</h1>
-				<p>
-					Easily keep track of your todos.
-					{this.storage &&
-						' Your todos will be stored even if you close the tab.'}
-				</p>
-				<input
-					type='text'
-					value={this.state.new}
-					onChange={event => this.setState({ new: event.target.value })}
-				/>
-				<button onClick={this._add}>Add</button>
-				<div>
+			<div>
+				<header className='header'>
+					<h1>Todo-list</h1>
+					<p>
+						Easily keep track of your todos.
+						{this.storage &&
+							' Your todos will be stored even if you close the tab.'}
+					</p>
+					<div>
+						<input
+							type='text'
+							value={this.state.new}
+							onChange={event => this.setState({ new: event.target.value })}
+						/>
+						<button onClick={this._add}>Add</button>
+					</div>
+				</header>
+				<section className='todos'>
 					{this.state.todos.map((todo, index) => (
-						<label className='container' key={index}>
-							{todo[0]}
+						<label className='todo' key={index}>
 							<input
 								type='checkbox'
 								checked={todo[1]}
 								onChange={event => this._toggleCheck(event, index)}
 							/>
 							<span className='checkmark' />
-							<button onClick={() => this._delete(index)}>Delete</button>
+							<p>{todo[0]}</p>
+							<button className='delete' onClick={() => this._delete(index)}>
+								X
+							</button>
 						</label>
 					))}
-				</div>
+				</section>
 			</div>
 		);
 	}
